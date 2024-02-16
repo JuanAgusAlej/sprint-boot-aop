1215d7ff4e91a3d0092e480301f713ba67c37b0c
Juan Agustin Alejandro <alejandro4juan@gmail.com>
2024-02-15 21:57:45 -0300

    First commit

---
+++ src/main/java/com/Alejandro4Juan/sprintbootaop/SprintBootAopApplication.java
@@ -0,0 +1,13 @@
+package com.Alejandro4Juan.sprintbootaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootAopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Alejandro4Juan/sprintbootaop/SprintBootAopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alejandro4Juan.sprintbootaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootAopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
